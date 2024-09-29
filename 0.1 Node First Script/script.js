8aa6802a473bd1bf3901b2c0cdfff8fccd945384
behan05 <behankrbth@outlook.com>
2024-09-29 19:23:05 +0530

    basic theoritically understand about node

---
+++ 0.1 Node First Script/script.js
@@ -0,0 +1,7 @@
+/*
+Node js is the javascript runtime environment which built by using chrome V8 javaScript Engine and C++ and it allow you to run script outside of browser. and it great to make web servers(Backend). 
+*/
+
+console.log("Hey there, JS!");
+
+
