de0b44258a76a82e03665a89e093c55fccdf1a98
behan05 <behankrbth@outlook.com>
2024-10-02 08:47:31 +0530

    update HTTP server to get request from the browser

--- 0.4 Built-In Modules/0.3 Http/script.js
+++ 0.4 Built-In Modules/0.3 Http/script.js
@@ -75,6 +75,6 @@ const server = http.createServer((req, res) => {
   }
 });
 
-server.listen(3000, () => {
-  console.log("Server started!");
+server.listen(300, () => {
+  console.log("Running Server!");
 });
