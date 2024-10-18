f546e5d51036512bee986c97c5345c7e946dbc59
behan05 <=>
2024-10-18 09:15:11 +0530

    make a resful API and also create Log with each request

--- RestFul API/index.js
+++ RestFul API/index.js
@@ -1,99 +1,135 @@
 const users = require("./MOCK_DATA.json");
 const express = require("express");
+const fs = require("fs");
+const path = require("path");
 
 const app = express();
 const PORT = 3000;
 
+// this middleware add upcoming form data in into body.
+app.use(express.urlencoded({ extended: true }));
 // Custom Middleware to convert object into JSON.
-// app.use(express.json());
-
+app.use(express.json());
 app.use((req, res, next) => {
-    if (req.method === "POST" || req.method === "PUT" || req.method === "PATCH") {
-        let data = "";
-
-        req.on("data", (chunk) => {
-            data += chunk;
-        }).on("end", () => {
-            try {
-                // try to parse data as JSON;
-                req.body = JSON.parse(data);
-                next();
-
-            } catch (error) {
-                // if parse failed! statusCode --> 400 for Bad Request.
-                return res.status(400).json({ error: "Invalid JSON" })
-            }
-        })
-    } else {
-        next();
-    }
+    const dt = new Date();
+    const logData = `1 --> ${dt.toLocaleDateString() + " : " + dt.toLocaleTimeString()}: ${req.method}: ${req.url}: ${req.ip} \n`
+    fs.appendFile(path.join(__dirname, "log.txt"), logData, (err) => {
+        if (err) {
+            console.log("Failed to created Log data!");
+        }
+    })
+    next();
 });
 
-app.get("/api/users/:id?", (req, res) => {
-    const id = req.params.id;
+// app.use((req, res, next) => {
+//     if (req.method === "POST" || req.method === "PUT" || req.method === "PATCH") {
+//         let data = "";
 
-    if (id) {
-        const user = users.find((user) => user.id === parseInt(id));
-        if (user) {
-            return res.status(200).json(user);
+//         req.on("data", (chunk) => {
+//             data += chunk;
+//         }).on("end", () => {
+//             try {
+//                 // try to parse data as JSON;
+//                 if (req.headers['content-type'] === "application/json") {
+//                     req.body = JSON.parse(data);
+//                 }
+//                 next();
+
+//             } catch (error) {
+//                 // if parse failed! statusCode --> 400 for Bad Request.
+//                 return res.status(400).json({ error: "Invalid JSON" })
+//             }
+//         })
+//     } else {
+//         next();
+//     }
+// });
+
+// Dynamic Route.
+app
+    .route("/api/users/:id?")
+
+    .get((req, res) => {
+        const id = req.params.id;
+
+        if (id) {
+            const user = users.find((user) => user.id === parseInt(id));
+            if (user) {
+                return res.status(200).json(user);
+            } else {
+                return res.status(404).json({ error: "user not found" })
+            }
         } else {
-            return res.status(404).json({ error: "user not found" })
+            return res.status(200).json(users);
         }
-    } else {
-        return res.status(200).json(users);
-    }
-})
+    })
+    // Edit an existing user.
+    .patch((req, res) => {
+        const id = parseInt(req.params.id);
+
+        const user = users.find(user => user.id === id);
+        if (user) {
+            // Update only the provided fields.
+            const { first_name, last_name, email, gender, job_title } = req.body;
+            if (first_name) user.last_name = first_name;
+            if (last_name) user.last_name = last_name;
+            if (email) user.email = email;
+            if (gender) user.gender = gender;
+            if (job_title) user.job_title = job_title;
+
+            return res.status(200).json({ message: "User updated successfully!", user: user });
+        } else {
+            return res.status(404).json({ error: "User not found" });
+        }
+    })
+
+    // Delete existing user.
+    .delete((req, res) => {
+        const id = parseInt(req.params.id);
+        if (id) {
+            const userIndex = users.findIndex(user => user.id === id);
+            if (userIndex !== -1) {
+                const [deletedUser] = users.splice(userIndex, 1);
+                return res.status(200).json({ message: "User Deleted!", deletedUser: deletedUser });
+            } else {
+                return res.status(404).json({ error: "user not found" })
+            }
+        } else {
+            return res.status(400).json({ error: "Invalid ID" });
+        }
+    })
 
 // Create new user.
 app.post("/api/users", (req, res) => {
-    const { id, first_name, last_name, email, gender, job_title } = req.body;
-    const data = {
-        id: id,
-        firstName: first_name,
-        lastName: last_name,
-        email: email,
-        gender: gender,
-        jobTitle: job_title
-    }
-    users.push(data);
-    res.status(200).json({ message: "New User Created!", user: data })
-})
+    // console.log(req.body);
 
-// Edit an existing user.
-app.patch("/api/users/:id?", (req, res) => {
-    const id = parseInt(req.params.id);
+    // const { id, first_name, last_name, email, gender, job_title } = req.body;
+    // const data = {
+    //     id: parseInt(id),
+    //     first_name,
+    //     last_name,
+    //     email,
+    //     gender,
+    //     job_title
+    // }
+    const newUser = req.body;
 
-    const user = users.find(user => user.id === id);
-    if (user) {
-        // Update only the provided fields.
-        const { first_name, last_name, email, gender, job_title } = req.body;
-        if (first_name) users.last_name = first_name;
-        if (last_name) users.last_name = last_name;
-        if (email) users.email = email;
-        if (gender) users.gender = gender;
-        if (job_title) users.job_title = job_title;
+    // spread Object.and join new ID.
+    users.push({ ...newUser, id: users.length + 1 });
 
-        return res.status(200).json({ message: "User updated successfully!", user: user });
-    } else {
-        return res.status(404).json({ error: "User not found" });
-    }
-})
-
-// Delete existing user.
-app.delete("/api/users/:id?", (req, res) => {
-    const id = parseInt(req.params.id);
-    if (id) {
-        const userIndex = users.findIndex(user => user.id === id);
-        if (userIndex !== -1) {
-            const deleteUser = users.splice(userIndex, 1);
-            return res.status(200).json({ message: "User Deleted!", deletedUser: deleteUser });
-        } else {
-            return res.status(404).json({ error: "user not found" })
+    // Write the updated users array back to the file
+    fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (err) => {
+        if (err) {
+            return res.status(500).json({ message: "Error saving user to file", error: err });
         }
-    } else {
-        return res.status(400).json({ error: "Invalid ID" });
-    }
+        else {
+            return res.status(200).json({ message: "New User Created!", user: newUser })
+        }
+    });
 })
 
-app.listen(PORT, () => console.log(`server started at port: ${PORT}`)
-)
+
+app.listen(PORT, () => console.log(`server started at port: ${PORT}`));
+
+
+
