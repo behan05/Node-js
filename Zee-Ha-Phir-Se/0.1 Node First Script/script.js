49394dbf561719bda519c5ed13ac3eb638cbeed8
behan05 <=>
2025-02-05 07:21:23 +0530

    recap node from scretch

---
+++ Zee-Ha-Phir-Se/0.1 Node First Script/script.js
@@ -0,0 +1,24 @@
+
+console.log("Hello i am learning NodeJs again! God Bless Me!");
+let number = 12;
+let name = "behan";
+let isLoggedIn = false;
+const obj = {
+    name: "Behan",
+    age: 25,
+    community: [{
+        dev: "dev.to",
+        github: "github.com/behan05"
+    }]
+}
+
+// for (const e of obj.community) {
+//     if(typeof e === "object") {
+//         console.log(Object.entries(e));
+//     }
+// }
+
+// console.log(name, number, isLoggedIn);
+
+
+
